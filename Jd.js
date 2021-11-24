da75a7ad8d839df186206237659a180a70cbc1cd
Prasenjeethowlader <94917470+Prasenjeethowlader@users.noreply.github.com>
2021-11-24 15:40:08 +0600

    Add files via upload

---
+++ Jd.js
@@ -0,0 +1,14 @@
+let username = document.querySelectorAll('h4');
+username.forEach(function(inputForce){
+  
+  inputForce.onclick=()=>{
+    
+    alert()
+    window.location.href='Suggest.html'
+    
+    
+    
+    
+  }
+  
+})
